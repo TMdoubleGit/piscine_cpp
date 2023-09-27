f82f683b3a1634796ad92f7cca9da449740b1b5b
Thomas MICHEL-WEIL <tmichel-@made-f0Ar12s5.clusters.42paris.fr>
2023-09-27 16:08:21 +0200

    adding cpp_09 ex02 tmp

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -6,8 +6,28 @@
 /*   By: tmichel- <tmichel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 09:46:02 by tmichel-          #+#    #+#             */
-/*   Updated: 2023/09/27 09:46:23 by tmichel-         ###   ########.fr       */
+/*   Updated: 2023/09/27 16:03:56 by tmichel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe()
+{
+}
+
+PmergeMe::PmergeMe(PmergeMe const & src)
+{
+	*this = src;
+}
+
+PmergeMe::~PmergeMe()
+{
+}
+
+PmergeMe & PmergeMe::operator=(PmergeMe const & rhs)
+{
+	(void)rhs;
+	return *this;
+}
+
--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: tmichel- <tmichel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 09:46:06 by tmichel-          #+#    #+#             */
-/*   Updated: 2023/09/27 09:46:17 by tmichel-         ###   ########.fr       */
+/*   Updated: 2023/09/27 14:09:37 by tmichel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 # define PMERGEME_HPP
 
 # include <iostream>
+
+class PmergeMe
+{
+	public:
+		PmergeMe();
+		PmergeMe(PmergeMe const & src);
+		~PmergeMe();
+		PmergeMe & operator=(PmergeMe const & rhs);
+};
 
 #endif
